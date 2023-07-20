ee9fd60477e24b5cb335000400b3be8dd1102fd0
dilshodbekkhamroev98@gmail.com <dilshodbekkhamroev98@gmail.com>
2023-07-20 12:33:00 +0500

    Add Program File

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Averagely
+{
+class Program
+{
+    static void Main()
+    {
+    Console.Write("Enter the first number: ");
+    decimal number1 = Convert.ToDecimal(Console.ReadLine());
+
+    Console.Write("Enter the second number: ");
+    decimal number2 = Convert.ToDecimal(Console.ReadLine());
+    
+    Console.Write("Enter the third number: ");
+    decimal number3 = Convert.ToDecimal(Console.ReadLine());
+    
+    Console.Write("Enter the forth number: ");
+    decimal number4 = Convert.ToDecimal(Console.ReadLine());
+    
+    Console.Write("Enter the fifth number: ");
+    decimal number5 = Convert.ToDecimal(Console.ReadLine());
+    
+    decimal average = (number1+number2+number3+number4+number5)/5;
+    Console.WriteLine("Average:  "+average);
+    }
+}
+}
